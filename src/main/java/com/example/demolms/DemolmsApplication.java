ea0f1a31d5559168d4248f4748b59e6b98b1b774
siwon <siwon8783@gmail.com>
2021-09-11 14:17:28 +0900

    demolms 초기화 설정

---
+++ src/main/java/com/example/demolms/DemolmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demolms/DemolmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
